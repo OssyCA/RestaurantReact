5c0a38f7e1e6b10feeae06fdf589a2c413217200
Ossy <drake@OSSMAN>
2025-09-28 19:26:47 +0200

    removed old mui components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
     >
       <Container maxWidth="lg">
         <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
+          <Grid size={{ xs: 12, md: 4 }}>
             <Typography
               variant="h6"
               component="h3"
@@ -43,7 +43,7 @@ const Footer = () => {
             </Typography>
           </Grid>
 
-          <Grid item xs={12} md={4}>
+          <Grid size={{ xs: 12, md: 4 }}>
             <Typography
               variant="h6"
               component="h3"
@@ -60,7 +60,7 @@ const Footer = () => {
             </Typography>
           </Grid>
 
-          <Grid item xs={12} md={4}>
+          <Grid size={{ xs: 12, md: 4 }}>
             <Typography
               variant="h6"
               component="h3"
